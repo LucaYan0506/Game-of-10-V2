34df5bb3ed75764fcd64e9d7789b0034613b50d6
LucaYan0506 <lucayan0506@gmail.com>
2025-08-24 17:53:38 +0100

    base structure of MCTS

--- backend/AI/MCTS.py
+++ backend/AI/MCTS.py
@@ -1,35 +1,23 @@
-from django.test import Client
 import math, random, json
-from api.utils import is_valid_action, discard_card, update_game_state
-from itertools import permutations
 from django.db import close_old_connections
 from asgiref.sync import async_to_sync
 from api.models import Game
+from api.game_models.game import GameLogic 
+from api.game_models.card import Card 
+from api.game_models.action import Action 
+from api.game_config import BOARD_HEIGHT, BOARD_WIDTH, CARDS_SIZE
 from copy import deepcopy
-import itertools
+import itertools, time
 
-empty_board  = [[None for _ in range(13)] for _ in range(13)]
+empty_board  = [[None for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
 
 # Const values
 C = 1/math.sqrt(2)
-CARDS_SIZE = 6
-
-"""
-action = [
-    {
-        id: ,
-        val: ,
-        i: ,
-        j: ,
-
-    }
-]
-"""
 
 class Game_State:
     def __init__(self, board=None, user_cards=None, score=0):
         self.board = board if board else empty_board
-        self.user_cards = user_cards if user_cards else []
+        self.user_cards:list[str] = user_cards if user_cards else []
         self.score = score
 
     def game_is_end(self):
@@ -40,46 +28,47 @@ class Game_State:
                           user_cards=self.user_cards[:],
                           score=self.score)
 
-    def _candidate_lines(self):
+    def _candidate_lines(self)->list[list[tuple[int,int]]]:
         # return all horizontal and vertical lines
         lines = []
-        for r in range(13):
-            lines.append([self.board[r][c] for c in range(13)])  # horizontal
-        for c in range(13):
-            lines.append([self.board[r][c] for r in range(13)])  # vertical
+        for i in range(BOARD_HEIGHT):
+            lines.append([(i,j) for j in range(13)])  # horizontal
+        for j in range(BOARD_WIDTH):
+            lines.append([(i,j) for i in range(13)])  # vertical
         return lines
 
-    def _find_blanks(self, line):
-        return [i for i, cell in enumerate(line) if cell == None]
+    def _find_blanks(self, line:list[tuple[int,int]])->list[tuple[int,int]]:
+        return [(i,j) for (i,j) in line if self.board[i][j] == None]
 
-    def _valid_fills(self, line, blanks):
+    def _valid_fills(self, blanks:list[tuple[int,int]])->list[Action]:
         # prune: only fill up to 3 blanks at a time
         if not blanks:
             return []
 
         max_fill = min(3, len(blanks), len(self.user_cards))
-        fills = []
+        potentialAction:list[Action] = []
 
         # try all combinations of cards for the selected number of blanks
         for num_fill in range(1, max_fill+1):
-            for cards_comb in itertools.combinations(self.user_cards, num_fill):
+            for permutation in itertools.permutations(range(CARDS_SIZE), num_fill):
                 # fill blanks sequentially
-                new_line = line[:]
+                cardsToPlace:list[Card] = []
 
 
                 # change this to what i have in utils
-                for idx, card in zip(blanks, cards_comb):
-                    new_line[idx] = card
-                fills.append(new_line)
+                for (i,j), cardID in zip(blanks, permutation):
+                    cardsToPlace.append(Card(i=i,j=j,val=self.board[i][j],id=cardID))
+                potentialAction.append(Action(cardsToPlace))
 
-        return fills
+        return potentialAction
 
     def get_potential_actions(self):
         actions = []
         for line in self._candidate_lines():
             blanks = self._find_blanks(line)
-            for action in self._valid_fills(line, blanks):
-                if is_valid_action(action, self.user_cards, self.board):
+            for action in self._valid_fills(blanks):
+                is_valid, _ = action.is_valid_action(self.user_cards)
+                if is_valid:
                     actions.append(action)
         return actions
 
@@ -91,7 +80,7 @@ class Node:
         self.game_state = game_state
         self.q = 0 # reward
         self.n = 0 # number of exploration
-        self.a = [] # action
+        self.a = Action([]) # action used to get from parent to this node
 
     def is_fully_expanded(self):
         pass #TODO
@@ -166,35 +155,47 @@ def uct_search(board):
     return best_child(root)
     
 
-def play(game_id):
+def play(game_id, log=False, is_creator = False):
     close_old_connections()  # Important for DB access in new thread
 
     game = Game.objects.get(game_id=game_id)
-    if game.creator_turn:
-        print("Not AI's turn")
+    game.refresh_from_db()
+    if game.creator_turn != is_creator:
+        if log:
+            print("Not AI's turn")
         return # not ai turn
     
-    print("AI is thinking...")
+    if log:
+        print("MCTS is thinking...")
+    time.sleep(2)
+    
     board = json.loads(game.board) 
-    my_cards = json.loads(game.opponent_cards)
-    card_placed = uct_search(my_cards, board).a
-    print("AI try to place cards at ", card_placed)
-
-    if is_valid_action(card_placed=card_placed, my_cards=my_cards, board=board):
-        print("Card placed, update DB")
-        update_game_state(card_placed, my_cards, game, False)
+    my_cards = json.loads(game.creator_cards if is_creator else game.opponent_cards)
+    action = uct_search(my_cards, board).a
+    
+    if log:
+        print("AI try to place cards at ", action.placed_cards)
+    is_valid, _ = action.is_valid_action(my_cards=my_cards, board=board)
+    game_logic = GameLogic(game)
+    
+    if is_valid:
+        if log:
+            print("Card placed, update DB")
+        game_logic.update(action, my_cards, is_creator_turn=is_creator)
     else:
-        print("Invalid action, AI is going to discard a random card")
+        if log:
+            print("Invalid action, AI is going to discard a random card")
         selectedCardIndex = random.randint(0, CARDS_SIZE - 1)
-        discard_card(game, my_cards, selectedCardIndex, False)
+        game_logic.discard(my_cards, selectedCardIndex, is_creator_turn=is_creator)
 
     # send websocket message 
-    from channels.layers import get_channel_layer
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        game_id,
-        {
-            'type': 'update',
-            'payload': 'ai_action_made'
-        }
-    )
+    if log: # new arg live, instead of using log
+        from channels.layers import get_channel_layer
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            game_id,
+            {
+                'type': 'update',
+                'payload': 'ai_action_made'
+            }
+)
