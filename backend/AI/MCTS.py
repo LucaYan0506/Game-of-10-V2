b0ecbcda84c0c9c02482aed2a1b87822fe6cf32c
LucaYan0506 <lucayan0506@gmail.com>
2025-08-26 20:16:50 +0100

    basic working MCTS done!

--- backend/AI/MCTS.py
+++ backend/AI/MCTS.py
@@ -1,12 +1,10 @@
 import math, random, json
 from django.db import close_old_connections
-from asgiref.sync import async_to_sync
 from api.models import Game
 from api.game_models.game import GameLogic 
 from api.game_models.game_state import GameState 
-from api.game_models.action import Action 
-from api.game_config import BOARD_HEIGHT, BOARD_WIDTH, CARDS_SIZE, EMPTY_BOARD
-from AI.hard_codedv1 import find_empty_spot
+from api.game_models.action import Action, ActionType
+from api.game_config import BOARD_HEIGHT, BOARD_WIDTH
 
 empty_board  = [[None for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
 
@@ -22,7 +20,7 @@ class Node:
         self.game_logic = GameLogic(game, is_simulation=True)
         self.q = 0 # reward
         self.n = 0 # number of exploration
-        self.a = Action([]) # action used to get from parent to this node
+        self.a = None # action used to get from parent to this node
 
     def is_fully_expanded(self):
         return len(self.tried_action) == 50 #TODO instead of 50 use a dynamic value
@@ -39,7 +37,7 @@ def UCB1(parent:Node, child:Node):
     return child.q / child.n + C * math.sqrt(2*math.log(parent.n))/(child.n + extra)
 
 def best_child(node:Node)->Node:
-    best_node = Node(None, GameState())
+    best_node = None
     best_node_ucb = -100000000
     for child in node.children:
         ucb = UCB1(node, child)
@@ -55,7 +53,11 @@ def expand(node:Node)->Node:
     for action in valid_actions:
         if action not in node.tried_action:
             untried_action.append(action)
+    
     # get untried action 
+    # # TODO include discard as a valid action
+    # if len(untried_action) == 0:
+    #     action = 
     action = untried_action[random.randint(0, len(untried_action) - 1)]
     newChild = Node(node, node.game_logic.game, is_creator=not node.is_creator)
     newChild.game_logic.update(action)
@@ -78,8 +80,11 @@ def default_policy(node: Node):
         # Note: randint is inclusive on both side
         action = valid_actions[random.randint(0, len(valid_actions) - 1)]  
         node.game_logic.update(action)
+    
+    node.game_logic.game.winner = 'creator' if node.game_logic.game.creator_point >= 20 else 'opponent'
 
-    return node.game_logic.game.score
+    user = 'creator' if node.is_creator else 'opponent'
+    return node.game_logic.game.winner == user
 
 def back_prop(node:Node, reward:int):
     while node is not None:
@@ -99,7 +104,7 @@ def uct_search(game:Game, is_creator:bool)->Action:
 
 def play(game_id, log=False, is_creator = False):
     close_old_connections()  # Important for DB access in new thread
-
+    
     game = Game.objects.get(game_id=game_id)
     game_logic = GameLogic(game,is_simulation=False)
     game.refresh_from_db()
@@ -110,35 +115,14 @@ def play(game_id, log=False, is_creator = False):
     
     if log:
         print("MCTS is thinking...")
-        # time.sleep(2)
     
-
-    board = json.loads(game.board) 
-    my_cards = json.loads(game.creator_cards if is_creator else game.opponent_cards)
     action = uct_search(game, is_creator=is_creator)
     
     if log:
-        print("AI try to place cards at ", action.placed_cards)
-    is_valid, _ = action.is_valid_action(my_cards=my_cards, board=board)
-    
-    if is_valid:
-        if log:
+        if action.type == ActionType.PLACE:
+            print("AI try to place cards at ", action.placed_cards)
             print("Card placed, update DB")
-        game_logic.update(action)
-    else:
-        if log:
-            print("Invalid action, AI is going to discard a random card")
-        selectedCardIndex = random.randint(0, CARDS_SIZE - 1)
-        game_logic.discard(selectedCardIndex)
+        elif action.type == ActionType.DISCARD:
+            print("No valid PLACE action found, discard a random card")
 
-    # send websocket message 
-    if log:
-        from channels.layers import get_channel_layer
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            game_id,
-            {
-                'type': 'update',
-                'payload': 'ai_action_made'
-            }
-)  
+    game_logic.update(action)
